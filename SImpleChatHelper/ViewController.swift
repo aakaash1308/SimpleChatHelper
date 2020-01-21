f4ff832dd7e7e3f07b1519e1620cf6546032de6a
aakaashkapoor <aakaash1308@gmail.com>
2020-01-20 17:24:45 -0800

    Initial Commit

---
+++ SImpleChatHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SImpleChatHelper
+//
+//  Created by Aakaash kapoor on 1/20/20.
+//  Copyright © 2020 Aakaash kapoor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
